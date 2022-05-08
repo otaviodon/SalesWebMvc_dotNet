a75f96135a19be3919c0552f5473c145872932e0
Otavio Moreira <otavio_mds@hotmail.com>
2022-05-08 20:51:52 -0300

    Seeding Service

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -9,17 +9,17 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public double Amount { get; set; }
-        public SalesStatus MyProperty { get; set; }
+        public SalesStatus Status { get; set; }
         public Seller Seller { get; set; }
 
         public SalesRecord() { }
 
-        public SalesRecord(int id, DateTime date, double amount, SalesStatus myProperty, Seller seller)
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
         {
             Id = id;
             Date = date;
             Amount = amount;
-            MyProperty = myProperty;
+            Status = status;
             Seller = seller;
         }
     }

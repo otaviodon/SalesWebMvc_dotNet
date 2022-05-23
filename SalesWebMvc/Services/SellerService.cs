3033a6a60a098ba8d75c977c9ab91fdf2228572c
Otavio Moreira <otavio_mds@hotmail.com>
2022-05-22 22:22:16 -0300

    tratamento de exceções para delete

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -37,9 +37,17 @@ namespace SalesWebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
+            
         }
 
         public async Task UpdateAsync(Seller obj)
@@ -53,7 +61,7 @@ namespace SalesWebMvc.Services
             try
             {
                 _context.Update(obj);
-                _context.SaveChangesAsync();
+                await _context.SaveChangesAsync();
             } 
             catch(DbUpdateConcurrencyException e)
             {
